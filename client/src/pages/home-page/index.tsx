1bab58d5c180739bcdb5c0f8af23c8b8c6d5c60b
Sithija Shehara <nsithijashehara@gmail.com>
2025-01-20 15:02:02 +0530

    home page UI

--- client/src/pages/home-page/index.tsx
+++ client/src/pages/home-page/index.tsx
@@ -1,5 +1,26 @@
+import {
+  BookOfTheWeek,
+  ContactUs,
+  LibraryCard,
+  LibraryHours,
+  UpcomingEvents,
+} from "../../features/landing";
 import "./index.css";
 
 export const HomePage = () => {
-  return <div className="page">Home Page</div>;
+  return (
+    <div className="page">
+      <div className="home-page-container">
+        <div className="home-page-left">
+          <BookOfTheWeek />
+          <UpcomingEvents />
+          <LibraryCard />
+        </div>
+        <div className="home-page-right">
+          <LibraryHours />
+          <ContactUs />
+        </div>
+      </div>
+    </div>
+  );
 };
